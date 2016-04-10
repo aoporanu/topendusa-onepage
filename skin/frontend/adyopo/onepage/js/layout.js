ec7f3bc2159042666a6e4b09c0e6acaf18261f8f
aoporanu <aoporanu@gmail.com>
2016-04-10 14:32:52 +0300

    Modifications to the czetnner ajax module and the checkout/cart templates

--- skin/frontend/adyopo/onepage/js/layout.js
+++ skin/frontend/adyopo/onepage/js/layout.js
@@ -252,6 +252,29 @@ var Layout = function () {
         jQuery(".quantity-up").html("<i class='fa fa-angle-up'></i>");
     }
 
+    var Checkout = function () {
+
+        return {
+            init: function () {
+
+                $('#checkout').on('change', '#checkout-content input[name="account"]', function() {
+
+                    var title = '';
+
+                    if ($(this).attr('value') == 'register') {
+                        title = 'Step 2: Account &amp; Billing Details';
+                    } else {
+                        title = 'Step 2: Billing Details';
+                    }
+
+                    $('#payment-address .accordion-toggle').html(title);
+                });
+
+            }
+        };
+
+    }();
+
     return {
         init: function () {
             //handlePromoBlock();
@@ -271,6 +294,7 @@ var Layout = function () {
             handleTheme();
             initImageZoom();
             initTouchspin();
+            Checkout();
         },
     };
 }();
